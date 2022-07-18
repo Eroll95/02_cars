e1a81953bc17740fbfb81ddf6da91a9bdccf118c
Karol Kowalski <karol.kowalski@smartepc.pl>
2022-07-18 13:32:12 +0200

    ADD all required models

--- models/car.py
+++ models/car.py
@@ -9,8 +9,54 @@ class EngineType(Enum):
     LPG = 3
 
 
+@unique
+class TyreType(Enum):
+    WINTER = 1
+    SUMMER = 2
+
+
+@unique
+class CarBodyColor(Enum):
+    BLACK = 1
+    SILVER = 2
+    WHITE = 3
+    RED = 4
+    BLUE = 5
+    GREEN = 6
+
+
+@unique
+class CarBodyType(Enum):
+    SEDAN = 1
+    HATCHBACK = 2
+    ESTATE = 3
+
+
 @dataclass
 class Engine:
     type: EngineType
     power: int
 
+
+@dataclass
+class Wheel:
+    model: str
+    size: int
+    type: TyreType
+
+
+@dataclass
+class CarBody:
+    color: CarBodyColor
+    type: CarBodyType
+    components: list[str]
+
+
+@dataclass
+class Car:
+    model: str
+    price: int
+    mileage: int
+    engine: Engine
+    carBody: CarBody
+    wheel: Wheel
