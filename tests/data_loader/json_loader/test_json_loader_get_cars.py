3393d979da4be4a9a86c201c5ace8507238a0062
Eroll95 <karkow19@gmail.com>
2022-12-12 13:29:29 +0100

    ADD TODO.txt file for noting all known issues with the app and TODOs

--- tests/data_loader/json_loader/test_json_loader_get_cars.py
+++ tests/data_loader/json_loader/test_json_loader_get_cars.py
@@ -6,6 +6,7 @@ from typing import Final
 
 cars_filename_path: Final[str] = r'app\resources\cars.json'
 
+
 class JsonLoaderImportData(unittest.TestCase):
     def test_load_correct_file(self):
         self.assertTrue(get_cars(cars_filename_path))
@@ -25,7 +26,7 @@ class JsonLoaderImportData(unittest.TestCase):
     @pytest.mark.xfail()
     def test_if_file_is_empty(self):
         empty_file_path = r'.\tests\test_files\empty_file.json'
-        assert os.path.getsize(cars_filename_path) == 0
+        assert os.path.getsize(empty_file_path) == 0
 
     @staticmethod
     def test_if_file_is_not_empty():
