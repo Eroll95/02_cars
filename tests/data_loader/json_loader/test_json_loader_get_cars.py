139443bf945c264f62ecedff713c500ff2087b28
Eroll95 <karkow19@gmail.com>
2022-12-12 13:23:43 +0100

    MODIFY Changed all absolute paths in test_json_loader_get_cars.py to relative

--- tests/data_loader/json_loader/test_json_loader_get_cars.py
+++ tests/data_loader/json_loader/test_json_loader_get_cars.py
@@ -4,30 +4,27 @@ import os
 from app.data_loader.json_loader import get_cars
 from typing import Final
 
-cars_filename_path: Final[str] = r'C:\Dev\kmprograms\praktyka\praktyka - python\09_OBIEKTY\02_cars\resources\cars.json'
-
-# TODO - ścieżki relatywne
-
+cars_filename_path: Final[str] = r'app\resources\cars.json'
 
 class JsonLoaderImportData(unittest.TestCase):
     def test_load_correct_file(self):
         self.assertTrue(get_cars(cars_filename_path))
 
     def test_load_one_example_object(self):
-        path = r'C:\Dev\kmprograms\praktyka\praktyka - python\09_OBIEKTY\02_cars\tests\test_files\test_car.json'
-        self.assertTrue(get_cars(relative_path))
+        test_car_path = r'.\tests\test_files\test_car.json'
+        self.assertTrue(get_cars(test_car_path))
 
     def test_load_wrong_data_format(self):
-        path = r'C:\Dev\kmprograms\praktyka\praktyka - python\09_OBIEKTY\02_cars\tests\test_files\wrong_format.json'
+        path = r'.\tests\test_files\wrong_format.json'
         self.assertRaises(ValueError, lambda: get_cars(path))
 
-    def test_load_empty_file(self):
-        path = r'C:\Dev\kmprograms\praktyka\praktyka - python\09_OBIEKTY\02_cars\tests\test_files\empty_file.json'
-        self.assertRaises(ValueError, lambda: get_cars(path))
+    def test_load_empty_file_throw_value_error(self):
+        empty_file_path = r'.\tests\test_files\empty_file.json'
+        self.assertRaises(ValueError, lambda: get_cars(empty_file_path))
 
     @pytest.mark.xfail()
     def test_if_file_is_empty(self):
-        empty_file_path = r'C:\Dev\kmprograms\praktyka\praktyka - python\09_OBIEKTY\02_cars\tests\test_files\empty_file.json'
+        empty_file_path = r'.\tests\test_files\empty_file.json'
         assert os.path.getsize(cars_filename_path) == 0
 
     @staticmethod
