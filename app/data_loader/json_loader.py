9da42d0fea4fb62b9ded6000695defb600302b8e
Eroll95 <karkow19@gmail.com>
2022-12-13 14:32:07 +0100

    MODIFY class ValidateWheelData now has complete validators

--- app/data_loader/json_loader.py
+++ app/data_loader/json_loader.py
@@ -1,5 +1,5 @@
 from app.model.car import Car
-from app.validator.validator import validate_car
+# from app.validator.validator import validate_car
 from app.validator.validator import ValidateWheelData as VMD
 import json
 from typing import Final, Any
@@ -7,18 +7,18 @@ from typing import Final, Any
 # LEGACY - for validating object models instead of dict from json
 
 
-def get_cars_legacy(filename: str) -> list['Car']:
-    with open(filename, 'r') as json_file:
-        try:
-            json_data = json.load(json_file)
-            cars = []
-            for data in json_data:
-                car = Car.of(data)
-                if validate_car(car):
-                    cars.append(car)
-            return cars
-        except Exception as e:
-            raise ValueError(e)
+# def get_cars_legacy(filename: str) -> list['Car']:
+#     with open(filename, 'r') as json_file:
+#         try:
+#             json_data = json.load(json_file)
+#             cars = []
+#             for data in json_data:
+#                 car = Car.of(data)
+#                 if validate_car(car):
+#                     cars.append(car)
+#             return cars
+#         except Exception as e:
+#             raise ValueError(e)
 
 
 def get_cars(filename: str) -> list[dict[str, Any]]:
@@ -27,11 +27,11 @@ def get_cars(filename: str) -> list[dict[str, Any]]:
             json_data = json.load(json_file)
             cars = []
             for data in json_data:
-                print(data['wheel']['type'])
                 car = VMD(data)
                 print(car)
-                if VMD.validate_wheel_new(car):
-                    print('yes')
+                if VMD.validate_wheel(car):
+                    car = Car.of(data)
+                    cars.append(car)
             return cars
         except Exception as e:
             raise ValueError(e)
@@ -42,4 +42,4 @@ if __name__ == '__main__':
     cars_dev_testfile_path: Final[str] = r'.\..\..\tests\test_files\test_car.json'
     car1 = get_cars(cars_dev_testfile_path)
 
-    # print(car1)
+    print(type(car1[0]))
--- app/validator/validator.py
+++ app/validator/validator.py
@@ -16,101 +16,97 @@ MILEAGE_MAX: Final[int] = 1000000
 # TODO - podzielić walidatory na klasy
 
 
-def validate_car(car: Car) -> bool:
-    return validate_basic_data(car) and validate_engine(car.engine) and validate_car_body(car.car_body) and validate_wheel(car.wheel)
-
-
-def validate_basic_data(car: Car) -> bool:
-    return validate_name(car) and validate_price(car) and validate_mileage(car)
-
-
-def validate_name(car: Car) -> bool:
-    return matches_regex(r'^[A-Z]+$', car.model)
-
-
-def validate_price(car: Car) -> bool:
-    return isinstance(car.price, Decimal) and value_in_between_range(0, PRICE_MAX, car.price)
-
-
-def validate_mileage(car: Car) -> bool:
-    return isinstance(car.mileage, int) and value_in_between_range(0, MILEAGE_MAX, car.mileage)
-
-
-# Validate Engine---------------------------------------------------
-
-def validate_engine(engine: Engine) -> bool:
-    return validate_engine_type(engine) and validate_engine_power(engine)
-
-
-def validate_engine_type(engine: Engine) -> bool:
-    return matches_regex(r'^[A-Z]+$', engine.type.name)
-
-
-def validate_engine_power(engine: Engine) -> bool:
-    return isinstance(engine.power, float) and value_in_between_range(0, 10000, engine.power)
-
-
-# Validate CarBody--------------------------------------------------
-
-def validate_car_body(car_body: CarBody) -> bool:
-    return validate_color(car_body) and validate_body_type(car_body) and validate_components(car_body)
-
-
-def validate_color(car_body: CarBody) -> bool:
-    return matches_regex(r'^[A-Z]+$', car_body.color.name)
-
-
-def validate_body_type(car_body: CarBody) -> bool:
-    return matches_regex(r'^[A-Z]+$', car_body.type.name)
-
-
-def validate_components(car_body: CarBody) -> bool:
-    if False in [item.isupper() for item in car_body.components]:
-        return False
-    if len([item for item, count in Counter(car_body.components).items() if count > 1]) > 0:
-        return False
-    return True
-
-
-def validate_wheel(wheel: Wheel) -> bool:
-    return validate_wheel_type(wheel) and validate_model(wheel) and validate_size(wheel)
-
-
-def validate_wheel_type(wheel: Wheel) -> bool:
-    return matches_regex(r'^[A-Z]+$', wheel.type.name)
-
-
-def validate_model(wheel: Wheel) -> bool:
-    return matches_regex(r'^[A-Z]+$', wheel.model)
-
-
-def validate_size(wheel: Wheel) -> bool:
-    return isinstance(wheel.size, int) and wheel.size > 0
+# def validate_car(car: Car) -> bool:
+#     return validate_basic_data(car) and validate_engine(car.engine) and validate_car_body(car.car_body) and validate_wheel(car.wheel)
+#
+#
+# def validate_basic_data(car: Car) -> bool:
+#     return validate_name(car) and validate_price(car) and validate_mileage(car)
+#
+#
+# def validate_name(car: Car) -> bool:
+#     return matches_regex(r'^[A-Z]+$', car.model)
+#
+#
+# def validate_price(car: Car) -> bool:
+#     return isinstance(car.price, Decimal) and value_in_between_range(0, PRICE_MAX, car.price)
+#
+#
+# def validate_mileage(car: Car) -> bool:
+#     return isinstance(car.mileage, int) and value_in_between_range(0, MILEAGE_MAX, car.mileage)
+#
+#
+# # Validate Engine---------------------------------------------------
+#
+# def validate_engine(engine: Engine) -> bool:
+#     return validate_engine_type(engine) and validate_engine_power(engine)
+#
+#
+# def validate_engine_type(engine: Engine) -> bool:
+#     return matches_regex(r'^[A-Z]+$', engine.type.name)
+#
+#
+# def validate_engine_power(engine: Engine) -> bool:
+#     return isinstance(engine.power, float) and value_in_between_range(0, 10000, engine.power)
+#
+#
+# # Validate CarBody--------------------------------------------------
+#
+# def validate_car_body(car_body: CarBody) -> bool:
+#     return validate_color(car_body) and validate_body_type(car_body) and validate_components(car_body)
+#
+#
+# def validate_color(car_body: CarBody) -> bool:
+#     return matches_regex(r'^[A-Z]+$', car_body.color.name)
+#
+#
+# def validate_body_type(car_body: CarBody) -> bool:
+#     return matches_regex(r'^[A-Z]+$', car_body.type.name)
+#
+#
+# def validate_components(car_body: CarBody) -> bool:
+#     if False in [item.isupper() for item in car_body.components]:
+#         return False
+#     if len([item for item, count in Counter(car_body.components).items() if count > 1]) > 0:
+#         return False
+#     return True
+#
+#
+# def validate_wheel(wheel: Wheel) -> bool:
+#     return validate_wheel_type(wheel) and validate_model(wheel) and validate_size(wheel)
+#
+#
+# def validate_wheel_type(wheel: Wheel) -> bool:
+#     return matches_regex(r'^[A-Z]+$', wheel.type.name)
+#
+#
+# def validate_model(wheel: Wheel) -> bool:
+#     return matches_regex(r'^[A-Z]+$', wheel.model)
+#
+#
+# def validate_size(wheel: Wheel) -> bool:
+#     return isinstance(wheel.size, int) and wheel.size > 0
 
 
 # Validate Wheel--------------------------------------------------------
 
-@dataclass
 class ValidateWheelData:
 
     # data: dict[str, Any]
-    def __init__(self, data: list[str, Any]):
+    def __init__(self, data: dict[str, Any]):
         self.data = data
 
     def __str__(self):
         return f'{self.data}'
 
-    def validate_wheel_new(self) -> bool:
-        return True
-        # return validate_wheel_type_new() # and validate_model(wheel) and validate_size(wheel)
+    def validate_wheel(self) -> bool:
+        return self.validate_wheel_type() and self.validate_model() and self.validate_size()
 
+    def validate_wheel_type(self) -> bool:
+        return matches_regex(r'^[A-Z]+$', self.data['wheel']['type'])
 
-    # def validate_wheel_type_new(self) -> bool:
-    #     pass
-        # return matches_regex(r'^[A-Z]+$', )
+    def validate_model(self) -> bool:
+        return matches_regex(r'^[A-Z]+$', self.data['wheel']['model'])
 
-    # def validate_model(self, wheel: Wheel) -> bool:
-    #     return matches_regex(r'^[A-Z]+$', wheel.model)
-    #
-    # def validate_size(self, wheel: Wheel) -> bool:
-    #     return isinstance(wheel.size, int) and wheel.size > 0
+    def validate_size(self) -> bool:
+        return isinstance(self.data['wheel']['size'], int) and self.data['wheel']['size'] > 0
