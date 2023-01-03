7d5710340e7693985ce035319f0f4992a23842e1
Eroll95 <karkow19@gmail.com>
2023-01-03 15:20:14 +0100

    ADD validator wheel data tests

--- tests/data/validator_obj_data.py
+++ tests/data/validator_obj_data.py
@@ -8,16 +8,16 @@ WHEEL_INFO_1 = ValidateWheelData({'wheel': {'type': 'SUMMER', 'model': 'BRIDGEST
 
 
 BASIC_INFO_2 = ValidateBasicData({'model': 'ALFA ROMEO', 'price': 200, 'mileage': '2000'})
-ENGINE_INFO_2 = ValidateEngineData({'engine': {'type': 'DIESEL', 'power': 150}})
+ENGINE_INFO_2 = ValidateEngineData({'engine': {'type': 'HYBR1D', 'power': 150}})
 BODY_INFO_2 = ValidateCarBodyData({'car_body': {'color': 'BLACK', 'type': 'SEDAN', 'components': ['LED HEADLIGHTS', 'led']}})
 WHEEL_INFO_2 = ValidateWheelData({'wheel': {'type': 'SUMMER', 'model': 'PIRELLI3', 'size': -18}})
 
 BASIC_INFO_3 = ValidateBasicData({'model': 'BM3W', 'price': '220', 'mileage': -2000})
-ENGINE_INFO_3 = ValidateEngineData({'engine': {'type': 'DIESEL', 'power': 150.0}})
+ENGINE_INFO_3 = ValidateEngineData({'engine': {'type': 1, 'power': -150.0}})
 BODY_INFO_3 = ValidateCarBodyData({'car_body': {'color': 'PINK', 'type': 'COUPE', 'components': ['BLUETOOTH', 'BLUETOOTH']}})
 WHEEL_INFO_3 = ValidateWheelData({'wheel': {'type': 'ALL-SEASON', 'model': 'pirelli', 'size': 18}})
 
 BASIC_INFO_4 = ValidateBasicData({'model': 'MERCEDES', 'price': '220', 'mileage': 26000})
-ENGINE_INFO_4 = ValidateEngineData({'engine': {'type': 'PETROL', 'power': 350.0}})
+ENGINE_INFO_4 = ValidateEngineData({'engine': {'type': 'GASOLINE', 'power': '350.0'}})
 BODY_INFO_4 = ValidateCarBodyData({'car_body': {'color': 'PINK', 'type': 'COUPE', 'components': []}})
 WHEEL_INFO_4 = ValidateWheelData({'wheel': {'type': 'ALL-SEASON', 'model': 'pirelli', 'size': 18}})
--- tests/model/validator/test_model_validator_basic_data.py
+++ tests/model/validator/test_model_validator_basic_data.py
@@ -5,12 +5,9 @@ from app.validator.validator import ValidateWheelData, ValidateBasicData
 
 class MyTestCase(unittest.TestCase):
 
-    def test_object_has_correct_wheel_type(self):
-        self.assertTrue(ValidateWheelData.validate_wheel(WHEEL_INFO_1))
-        self.assertTrue(ValidateWheelData.validate_wheel(WHEEL_INFO_2))
-
     def test_object_has_correct_name(self):
         self.assertTrue(ValidateBasicData.validate_name(BASIC_INFO_1))
+        self.assertTrue(ValidateWheelData.validate_wheel(WHEEL_INFO_1))
 
     def test_object_has_correct_price(self):
         self.assertTrue(ValidateBasicData.validate_price(BASIC_INFO_1))
