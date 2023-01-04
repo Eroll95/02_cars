8e7a711dc472a368721bac6bae367a1a93907739
Eroll95 <karkow19@gmail.com>
2023-01-04 11:39:42 +0100

    ADD validator engine and corrected basic data tests

--- tests/model/validator/test_model_validator_basic_data.py
+++ tests/model/validator/test_model_validator_basic_data.py
@@ -1,16 +1,43 @@
+import pytest
 import unittest
-from tests.data.validator_obj_data import BASIC_INFO_1, WHEEL_INFO_1, WHEEL_INFO_2
-from app.validator.validator import ValidateWheelData, ValidateBasicData
+from tests.data.validator_obj_data import BASIC_INFO_1, BASIC_INFO_2, BASIC_INFO_3, BASIC_INFO_4
+from app.validator.validator import ValidateBasicData
 
 
-class MyTestCase(unittest.TestCase):
+class ValidatorValidateBasicData(unittest.TestCase):
+
+    # TESTS Name
 
     def test_object_has_correct_name(self):
         self.assertTrue(ValidateBasicData.validate_name(BASIC_INFO_1))
-        self.assertTrue(ValidateWheelData.validate_wheel(WHEEL_INFO_1))
+
+    def test_object_has_incorrect_name_contains_whitespaces(self):
+        self.assertFalse(ValidateBasicData.validate_name(BASIC_INFO_2))
+
+    def test_object_has_incorrect_name_contains_numbers(self):
+        self.assertFalse(ValidateBasicData.validate_name(BASIC_INFO_3))
+
+    # TESTS Price
 
     def test_object_has_correct_price(self):
         self.assertTrue(ValidateBasicData.validate_price(BASIC_INFO_1))
 
-    def test_object_has_correct_price_instance(self):
-        self.assertIsInstance(BASIC_INFO_1.data['price'], int)
+    def test_object_has_incorrect_price_contains_float(self):
+        self.assertFalse(ValidateBasicData.validate_price(BASIC_INFO_2))
+
+    def test_object_has_incorrect_price_contains_string(self):
+        self.assertFalse(ValidateBasicData.validate_price(BASIC_INFO_3))
+
+    def test_object_has_incorrect_price_contains_negative_decimal(self):
+        self.assertFalse(ValidateBasicData.validate_price(BASIC_INFO_4))
+
+    # TESTS Mileage
+
+    def test_object_has_correct_mileage(self):
+        self.assertTrue(ValidateBasicData.validate_mileage(BASIC_INFO_1))
+
+    def test_object_has_incorrect_mileage_contains_string(self):
+        self.assertFalse(ValidateBasicData.validate_mileage(BASIC_INFO_2))
+
+    def test_object_has_incorrect_mileage_contains_negative_value(self):
+        self.assertFalse(ValidateBasicData.validate_mileage(BASIC_INFO_3))
